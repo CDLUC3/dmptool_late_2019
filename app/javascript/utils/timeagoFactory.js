ffc856651b97dd670efb636d6ab2cf7fc36be9b9
briri <brian.riley@ucop.edu>
2019-05-10 09:56:54 -0700

    updated TimeAgo js to use en_US

--- app/javascript/utils/timeagoFactory.js
+++ app/javascript/utils/timeagoFactory.js
@@ -1,15 +1,15 @@
-import enGB from '../locale/en_GB/timeago';
+import enUS from '../locale/en_US/timeago';
 
 /* global timeago */
 const TimeagoFactory = (() => {
-  timeago.register('en_GB', enGB);
+  timeago.register('en_US', enUS);
   /*
     @param el - DOM element
     @returns
   */
   return {
     render: (el) => {
-      timeago().render(el, 'en_GB');
+      timeago().render(el, 'en_US');
     },
   };
 })();
